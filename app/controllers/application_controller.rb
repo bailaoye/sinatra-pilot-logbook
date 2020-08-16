3952ae64848b10b482bb3fbb01f38e8bc1d00c08
bailaoye <jeffbai996@gmail.com>
2020-08-16 23:40:55 +0800

    attempting to fix config.ru

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
-require './config/environment'
+#require './config/environment'
 
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret
+    set :session_secret, "password"
   end
 
   get '/' do
-    #test
+    "Hello World"
   end
 
 end 
